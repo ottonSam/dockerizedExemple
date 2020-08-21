2ce789ff4c34e482b257a26925bdad7013622114
samieel <sotton.maia@gmail.com>
2020-08-21 01:11:57 -0300

    reduce react app

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -8,10 +6,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>reduce react</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          relax bro your fine
         </p>
       </div>
     );
